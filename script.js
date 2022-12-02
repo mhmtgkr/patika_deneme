fb63ed9162fe61faa89716512e4f94f06e5eb0d0
Ege <mehmetegekar00@gmail.com>
2022-12-03 01:22:07 +0300

    tüm dosyalar eklendi

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+let left =document.getElementById("left"),
+right =document.getElementById("right"),
+container =document.getElementById("container");
+
+left.addEventListener("mouseenter",()=>{
+    container.classList.add("hover-left")
+})
+
+left.addEventListener("mouseleave",()=>{
+    container.classList.remove("hover-left")
+})
+
+right.addEventListener("mouseenter",()=>{
+    container.classList.add("hover-right")
+})
+
+right.addEventListener("mouseleave",()=>{
+    container.classList.remove("hover-right")
+})
+*/
